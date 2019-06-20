53ce88164bfc6ed35ce5835e9c9529151ad82304
IJRA <ivanjoelramirez@gmail.com>
2019-06-20 16:18:10 -0500

    Se agregan etiquetas HTML5 al index.php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
 	<title>Plus</title>
 </head>
 <body>
+	<header></header>
+	<nav></nav>
 	<h1>Bienvenido Extraño</h1>
 	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur odio justo, ultrices ut sollicitudin viverra, bibendum ac magna. Ut eu rutrum magna. Morbi gravida leo id turpis venenatis, id pulvinar tortor laoreet. Suspendisse dictum porta efficitur. Phasellus vestibulum enim a leo lacinia aliquam. Mauris sed ipsum et ipsum tempor porttitor. Aliquam iaculis, tellus quis dapibus malesuada, odio urna ultrices tortor, a posuere dui erat sed nunc. Nullam semper a urna non mattis. Quisque blandit in quam ut ultrices. Proin sit amet velit eget nibh pretium convallis eget at libero. Proin et semper dolor, nec tincidunt risus. Curabitur mollis tempus maximus. Aenean nec imperdiet neque, a condimentum lectus.</p>
+	<footer></footer>
 </body>
 </html>
